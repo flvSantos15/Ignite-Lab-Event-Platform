8a5f736bba21d1be8c05a170f67a655045015f15
flvSantos15 <flvSantos300@gmail.com>
2023-05-14 12:42:48 -0300

    feat: header + sidebar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,51 @@
+import { gql, useQuery } from '@apollo/client'
+import { Lesson } from './Lesson'
+
+const GET_LESSONS_QUERY = gql`
+  query MyQuery {
+    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+      id
+      slug
+      title
+      availableAt
+      lessonType
+      description
+    }
+  }
+`
+
+interface GetLessonQueryResponse {
+  lessons: {
+    id: string
+    slug: string
+    title: string
+    availableAt: string
+    lessonType: 'live' | 'class'
+    description: string
+  }[]
+}
+
+export function Sidebar() {
+  const { data } = useQuery<GetLessonQueryResponse>(GET_LESSONS_QUERY)
+  return (
+    <aside className="xl:w-[348px] bg-gray-700 p-6 border-l border-gray-600">
+      <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
+        Cronograma de aulas
+      </span>
+
+      <div className="flex flex-col gap-8">
+        {data?.lessons.map((lesson) => {
+          return (
+            <Lesson
+              key={lesson.id}
+              title={lesson.title}
+              slug={lesson.slug}
+              availableAt={new Date(lesson.availableAt)}
+              type={lesson.lessonType}
+            />
+          )
+        })}
+      </div>
+    </aside>
+  )
+}
