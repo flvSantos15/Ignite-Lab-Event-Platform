abfb04b3c36847078d42cb8331465342c8ee33f4
flvSantos15 <flvSantos300@gmail.com>
2023-05-14 18:49:12 -0300

    feat: router + video

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import { Event } from './pages/Event'
+import { ApolloProvider } from '@apollo/client'
+import { client } from './lib/apollo.ts'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
-  return <Event />
+  return (
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
+  )
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+
 import './styles/global.css'
-import { ApolloProvider } from '@apollo/client'
-import { client } from './lib/apollo.ts'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <App />
   </React.StrictMode>
 )
